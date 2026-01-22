397c6627849a9d088cdb443b9d63f33c5adb8a94
LuanODias <luoliveidias@gmail.com>
2026-01-22 13:50:22 -0300

    feat: add estoque_movimentacoes repository

--- repositories/estoque.repository.ts
+++ repositories/estoque.repository.ts
@@ -3,15 +3,15 @@ import { estoque } from '@/generated/prisma/client';
 
 export const findAll = async (): Promise<estoque[]> => {
     return prisma.estoque.findMany();
-}
+};
 
 export const findById = async (id: string): Promise<estoque | null> =>{
     return prisma.estoque.findUnique({
         where: {
             id,
         }
-    })
-}
+    });
+};
 
 export const update = async (id: bigint, data: Partial<Omit<estoque, 'id' | 'atualizado_em'>>): Promise<estoque> => {
   return prisma.estoque.update({
