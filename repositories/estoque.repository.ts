1608e6918550ea4b4cacf0a2644ab605a3095460
LuanODias <luoliveidias@gmail.com>
2026-01-22 16:38:38 -0300

    fix: remove estoque repository update logic

--- repositories/estoque.repository.ts
+++ repositories/estoque.repository.ts
@@ -1,23 +1,14 @@
-import prisma from '@/lib/db';
-import { estoque } from '@/generated/prisma/client';
+import prisma from "@/lib/db";
+import { estoque } from "@/generated/prisma/client";
 
 export const findAll = async (): Promise<estoque[]> => {
-    return prisma.estoque.findMany();
+  return prisma.estoque.findMany();
 };
 
-export const findById = async (id: bigint): Promise<estoque | null> =>{
-    return prisma.estoque.findUnique({
-        where: {
-            id,
-        }
-    });
-};
-
-export const update = async (id: bigint, data: Partial<Omit<estoque, 'id' | 'atualizado_em'>>): Promise<estoque> => {
-  return prisma.estoque.update({
-    where: { id },
-    data,
+export const findById = async (id: bigint): Promise<estoque | null> => {
+  return prisma.estoque.findUnique({
+    where: {
+      id,
+    },
   });
 };
-
-
